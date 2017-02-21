0896501fd02db51debb019940be65ca94135d35f
Clayton Petty <claytondpetty@gmail.com>
2017-02-20 23:50:37 -0600

    working

--- p1.py
+++ p1.py
@@ -26,17 +26,36 @@ def gen_two_bytes():
     hex_vals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
     for val1 in hex_vals:
         for val2 in hex_vals:
-            yield '\\x' + val1 + val2
+            hex_val = bytearray.fromhex(val1 + val2)
+            yield str(hex_val)
 
 def main():
-    for two_bytes in gen_two_bytes():
-        i = pydasm.get_instruction(two_bytes, pydasm.MODE_32)
+    for two_bytes1 in gen_two_bytes():
+        instruction = two_bytes1
+        i = pydasm.get_instruction(instruction, pydasm.MODE_32)
         if i:
-            print two_bytes, i.length
+            print instruction, 'LENGTH:', len(instruction), 'RESULT LENGTH:', i.length
         else:
-            print two_bytes, 'INVALID'
+            print instruction, 'INVALID'
+    
+    '''for two_bytes1 in gen_two_bytes():
+        for two_bytes2 in gen_two_bytes():
+            instruction = two_bytes1 + two_bytes2
+            i = pydasm.get_instruction(instruction, pydasm.MODE_32)
+            if i:
+                print instruction, 'LENGTH:', instruction, 'RESULT LENGTH:', i.length
+            else:
+                print instruction, 'INVALID'
+    '''
+            
 
      
 
 if __name__=='__main__':
     main()
+    i = pydasm.get_instruction(buffer, pydasm.MODE_32)
+    print '-----------'
+    print i.length
+    j = pydasm.get_instruction('\\x0F' + '\\x08' + '\\x8F' + '\\x00' + '\\x5D' + '\\xDE', pydasm.MODE_32)
+    print j.length
+
