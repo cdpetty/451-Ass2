463c868b5a00143eab06546ac8bbc446906929cb
Brad Swain <bradleyswain1@gmail.com>
2017-02-21 23:55:59 -0600

    even closer to a linear disassembler

--- p2.py
+++ p2.py
@@ -8,14 +8,23 @@ print hex_str
 buffer = str(bytearray.fromhex(hex_str))
 start = 0
 end = 1
+streak = 0
+streak_arr = []
 while end < len(buffer):
-	i = pydasm.get_instruction(buffer[start:end], pydasm.MODE_32)
-	print 'instr:', pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, 0)
+	i = pydasm.get_instruction('', pydasm.MODE_32)
 	print 'hex:', ' '.join([format(c, 'x') for c in bytearray(buffer[start:end])])
-	print 'length:', i.length
-	print '----'
-	start += 1
+	if i.length != end - start:
+		print 'OFF TRACK'
+		streak += 1
+	else:
+		print 'instr:', pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, 0)
+		print 'length:', i.length
+		start += i.length
+		streak_arr.append(streak)
+		streak = 0
 	if not i:
-		print 'test'
-		break
-	end += i.length
+		print 'BAD'
+	print '----'
+	end += 1
+
+
