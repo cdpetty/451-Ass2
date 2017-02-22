7f2cbdbb653d9323ff55543d55bea55b805c85b5
Brad Swain <bradleyswain1@gmail.com>
2017-02-21 23:38:22 -0600

    change p2 to linear disassemble almost

--- p2.py
+++ p2.py
@@ -6,11 +6,16 @@ with open('example.hex', 'r') as file:
 print hex_str
 
 buffer = str(bytearray.fromhex(hex_str))
-offset = 0
-
-while offset < len(buffer):
-	i = pydasm.get_instruction(buffer[offset:], pydasm.MODE_32)
-	print pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, 0)
+start = 0
+end = 1
+while end < len(buffer):
+	i = pydasm.get_instruction(buffer[start:end], pydasm.MODE_32)
+	print 'instr:', pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, 0)
+	print 'hex:', ' '.join([format(c, 'x') for c in bytearray(buffer[start:end])])
+	print 'length:', i.length
+	print '----'
+	start += 1
 	if not i:
+		print 'test'
 		break
-	offset += i.length
+	end += i.length
