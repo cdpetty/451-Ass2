4f0a81029abc1ad1563a331a2ffcf305029d2b39
Brad Swain <bradleyswain1@gmail.com>
2017-03-06 15:46:16 -0600

    format output

--- p2.py
+++ p2.py
@@ -2,31 +2,35 @@ import pydasm
 
 with open('example.hex', 'r') as file:
 	hex_str = file.read()
-
+print '----INPUT----'
 print hex_str
+print '--END INPUT--'
 
 buffer = str(bytearray.fromhex(hex_str))
 start = 0
 end = 1
 streak = 0
 streak_arr = []
+print_data = []
 while end <= len(buffer):
 	i = pydasm.get_instruction(buffer[start:end], pydasm.MODE_32)
-	print 'hex:', ' '.join([format(c, 'x') for c in bytearray(buffer[start:end])])
+	hex_str = ' '.join([format(c, 'x') for c in bytearray(buffer[start:end])])
+	if not i:
+		print_data.append([hex_str, 'BAD'])
 	if i.length != end - start:
-		print 'OFF TRACK'
 		streak += 1
 	else:
-		print 'instr:', pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, 0)
-		print 'length:', i.length
+		print_data.append([hex_str, pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, 0)])
+		#print 'length:', i.length
 		start += i.length
 		streak_arr.append(streak)
 		streak = 0
-	if not i:
-		print 'BAD'
-	print '----'
 	end += 1
 
-print 'Largest streak:', max(streak_arr)
-print 'Avg streak:', sum(streak_arr)/len(streak_arr)
-print 'Num Streaks:', len(streak_arr)
+for row in print_data:
+	print("{: <20} {: >30}".format(*row))
+
+#I don't think these are actually relevant to this assignmnet...
+#print 'Largest streak:', max(streak_arr)
+#print 'Avg streak:', sum(streak_arr)/len(streak_arr)
+#print 'Num Streaks:', len(streak_arr)
